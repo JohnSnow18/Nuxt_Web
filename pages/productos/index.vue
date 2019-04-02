fc8f20ca486093f4bdf86ea5b8502b0fbb8b814d
JohnSnow18 <jhonjajoy1803@gmail.com>
2019-04-02 15:12:59 -0500

    Nuxt_Web

---
+++ pages/productos/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1 style="margin-left: 35%">Listado de productos</h1>
+
+    <div style="row mt-2">
+      <div style="col-sm-12">
+        <b-table striped hover :items="items" :fields="fields"></b-table>
+      </div>
+    </div>
+
+    <div style="margin-left:42%">
+      <br>
+      <br>
+      <b-button href="/productos/nuevo" type="button" class="btn btn-success">Nuevo producto</b-button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fields: [
+        "Imagen",
+        "Nombre",
+        "Precio",
+        "Cantidad",
+        "Acciones"
+        /*items:
+           { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+          { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+          { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+          { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' } */
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
+
+ 
---
+++ pages/productos/nuevo.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1 style="margin-left: 40%">Registrar Producto</h1>
+    <div class="row">
+      <div class="col-sm-12">
+        <b-form-group label="Imagen" label-for="imagen">
+          <b-form-file id="Imagen" placeholder="Cargar Imagen" drop-placeholder="Cargar Imagen"/>
+        </b-form-group>
+
+        <b-form-group label="Nombre" label-for="nombre">
+          <b-form-input id="Nombre" type="text" required placeholder="Nombre producto"></b-form-input>
+        </b-form-group>
+
+        <b-form-group label="Precio" label-for="precio">
+          <b-form-input id="Precio" type="text" required placeholder="Precio producto"></b-form-input>
+        </b-form-group>
+
+        <b-form-group label="Cantidad" label-for="cantidad">
+          <b-form-input id="Cantidad" type="number" required placeholder="Cantidad producto"></b-form-input>
+        </b-form-group>
+
+        <div style="margin-left: 45%">
+          <b-button type="volver" variant="dark" href="../productos">Volver</b-button>
+          <b-button type="guardar" variant="primary">Guardar</b-button>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+};
+</script>
+
+<style>
+</style>
